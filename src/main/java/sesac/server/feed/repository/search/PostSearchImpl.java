c446426683f3315fe3dde0ff826c43380e9c35b1
rtg <rtg1539@naver.com>
2024-08-31 21:09:02 +0900

    refactor: 캠퍼스 피드 목록 수정

--- src/main/java/sesac/server/feed/repository/search/PostSearchImpl.java
+++ src/main/java/sesac/server/feed/repository/search/PostSearchImpl.java
@@ -3,23 +3,19 @@ package sesac.server.feed.repository.search;
 import static sesac.server.feed.entity.QPost.post;
 import static sesac.server.user.entity.QUser.user;
 
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 import sesac.server.feed.dto.PostResponse;
 import sesac.server.feed.dto.QPostResponse;
-import sesac.server.feed.entity.Post;
 
 @RequiredArgsConstructor
 public class PostSearchImpl implements PostSearch {
 
     private final JPAQueryFactory queryFactory;
 
-    public Page<PostResponse> searchPost(Pageable pageable) {
+    public List<PostResponse> searchPost(Pageable pageable) {
 
         List<PostResponse> posts = queryFactory
                 .select(new QPostResponse(
@@ -28,7 +24,9 @@ public class PostSearchImpl implements PostSearch {
                         post.title,
                         post.content,
                         post.createdAt,
-                        post.image
+                        post.image,
+                        post.likesCount,
+                        post.replyCount
                 ))
                 .from(post)
                 .join(post.user, user)
@@ -36,10 +34,12 @@ public class PostSearchImpl implements PostSearch {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        JPAQuery<Post> countQuery = queryFactory
+        return posts;
+
+        /*JPAQuery<Post> countQuery = queryFactory
                 .select(post)
                 .from(post);
 
-        return PageableExecutionUtils.getPage(posts, pageable, countQuery::fetchCount);
+        return PageableExecutionUtils.getPage(posts, pageable, countQuery::fetchCount);*/
     }
 }
